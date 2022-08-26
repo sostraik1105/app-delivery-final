7a37a2c09d886f230000f8e7e0ca8e65aa5feb7d
daniel puchuri <sostraik1105@gmail.com>
2022-08-26 15:35:26 -0500

    btn start

---
+++ app/src/main/java/com/example/myapplication/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ConstraintLayout startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
